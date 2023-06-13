de197b125565c7d1d4c42c7213705f76c35a59b4
Satyam Kumar~ <satyam.kumar@revdau.com>
2023-06-13 20:04:19 +0530

    Demo

---
+++ src/main/java/com/hms/patient/PatientModuleApplication.java
@@ -0,0 +1,13 @@
+package com.hms.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hms/patient/PatientModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
